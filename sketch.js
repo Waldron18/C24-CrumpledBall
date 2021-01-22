f5f2c799daf426ee6a417564a464c40921ad995e
Waldron18 <75988657+Waldron18@users.noreply.github.com>
2021-01-22 12:05:03 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,54 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var box1, box2, box3;
+var paper,ground;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	box1 = new Dustbin(600,650,180,20);
+	box2 = new Dustbin(520,625,20,100);
+	box3 = new Dustbin(700,625,20,100);
+	paper = new Paper(100,640,10);
+	ground = new Ground(400,680,800,40);
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  box1.display();
+  box2.display();
+  box3.display();
+  paper.display();
+  ground.display();
+  drawSprites();
+ 
+}
+
+function keyPressed() {
+	if (keyCode === UP_ARROW) {
+		 Body.applyForce(paper.body,paper.body.position,{x:15, y:-15});
+	}
+	 }
+   
+
+
+
